bfffd8320f1eb8ddc1dd1c32c5419486ee67835e
snidelyhazel <30618085+snidelyhazel@users.noreply.github.com>
2018-11-26 02:32:06 -0800

    [feat] First pass of representative lookup by zipcode
    Includes:
    + temporary button to trigger representative search
    + popup window to display results of representative search

--- public/pgscript.js
+++ public/pgscript.js
@@ -16,3 +16,30 @@ document.getElementById("is-in-group").addEventListener("change", function() {
 document.getElementById("is-in-union").addEventListener("change", function() {
   document.getElementById("union-row").style.display = (this.value == "yes") ? "" : "none";
 })
+
+// Find representatives by zipcode
+
+// Show popup window with House of Representatives search results
+function repSearch()
+{
+  var left = screen.width - 775;
+  var top = 0;
+  var w = window.open("", "popupWindow", "width=775, height=" + screen.height + ", scrollbars=yes, left=" + left + ", top=" + top + "");
+  w.location.href = "https://ziplook.house.gov/htbin/findrep_house?ZIP=" + ($("#zipcode").val());
+}
+
+$(document).ready(function()
+{
+  // Button click triggers popup window
+  $("#ziplook").on("click", repSearch);
+  
+  // Enter key triggers popup window
+  $('#zipcode').keypress(function(event)
+  {
+    // Enter key pressed
+     if(event.which == 13)
+     {
+       repSearch();
+     }
+   });
+});
