576955bbd6863b992fae80520be34cc040273a31
snidelyhazel <pg+snidelyhazel@users.noreply.github.com>
2018-11-26 00:32:47 -0800

    [feat] Initial Node server setup
    Includes:
    + Node server script
    + Node package configuration

---
+++ pgserver.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var fs = require('fs');
+
+app.get('/', function (req, res)
+{
+  fs.readFile('progressguide.html', 'utf8', function(err, data) {
+      if (err) throw err;
+      res.send(data)
+      console.log(data);
+  });
+})
+
+app.listen(3000)
