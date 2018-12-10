a3b9733bc0c14eb4075333a204edc5bff61c70a3
snidelyhazel <30618085+snidelyhazel@users.noreply.github.com>
2018-12-09 22:35:06 -0800

    [feat] Implement representative search on results page
    Incl:
    + add repsearch text to results page
    + remove repsearch from survey page
    + reimplement repsearch with Handlebars

--- public/progressaction.js
+++ public/progressaction.js
@@ -3,3 +3,18 @@ function openJoin(anchor, event)
   popupWindow(anchor.href);
   event.preventDefault();
 }
+
+// Find representatives by zipcode
+
+// Show popup window with House of Representatives search results
+function repSearch(event)
+{
+  //popupWindow("https://ziplook.house.gov/htbin/findrep_house?ZIP=" + ($("#zipcode").val()));
+  popupWindow($(event.target).attr('href'));
+}
+
+$(document).ready(function(event)
+{
+  // Button click triggers popup window
+  $("#ziplook").on("click", repSearch);
+});
